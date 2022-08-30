6210048332beb25759cb944601361fffd2c98fde
Eli Entelis <eli.entelis@hexagon.com>
2022-08-31 00:48:48 +0300

    api routes related to chapter & tests

---
+++ tests/app_tests.py
@@ -0,0 +1,103 @@
+import json
+import unittest
+from flask_jwt_extended import create_access_token
+from app import app
+from unittest import mock
+from bson.objectid import ObjectId
+from pathlib import Path
+from tests.test_data.chapters_data import mock_chapters_data, mock_chapter_data
+
+
+def mock_request_info():
+    access_token = create_access_token('test')
+    headers = {
+        'Authorization': 'Bearer {}'.format(access_token)
+    }
+    data = {
+        'key1': 'val1',
+        'key2': 'val2'
+    }
+    user = {'username': 'test', 'role': 'admin'}
+
+    return headers, data, user
+
+
+class AppTests(unittest.TestCase):
+    def setUp(self):
+        base_path = Path(__file__).parent
+        file_path = (base_path / "test_data/.jwt_secret_key_test").resolve()
+        self._client = app.test_client()
+        with open(file_path) as f:
+            lines = f.readlines()
+        app.config['JWT_SECRET_KEY'] = str(lines)
+
+    @mock.patch('app._db_controller')
+    def test_getChapters_success(self, mock_db_controller):
+        mock_db_controller.find.return_value = mock_chapters_data()
+        result = self._client.get('/api/v1/chapters')
+        self.assertEqual(200, result.status_code)
+        self.assertGreater(len(result.data), 0)
+
+    @mock.patch('app._db_controller')
+    def test_getChapter_success(self, mock_db_controller):
+        mock_db_controller.find_one.return_value = mock_chapter_data()
+        result = self._client.get(f'/api/v1/chapter/{ObjectId()}')
+        self.assertEqual(200, result.status_code)
+        self.assertGreater(len(result.data), 0)
+
+    @mock.patch('app._db_controller')
+    def test_getChapter_failure(self, mock_db_controller):
+        mock_db_controller.find_one.return_value = None
+        result = self._client.get(f'/api/v1/chapter/{ObjectId()}')
+        self.assertEqual(404, result.status_code)
+
+    @mock.patch('app._db_controller')
+    def test_updateChapter_success(self, mock_db_controller):
+        with app.app_context():
+            headers, data, user = mock_request_info()
+            mock_db_controller.find_one.return_value = user
+            update_one_result = mock_db_controller.update_one.return_value
+            update_one_result.matched_count = 1
+            result = self._client.put(f'/api/v1/chapter/{str(ObjectId())}', headers=headers, data=json.dumps(data),
+                                      content_type='application/json',
+                                      )
+        self.assertEqual(202, result.status_code)
+
+    @mock.patch('app._db_controller')
+    def test_updateChapter_failure(self, mock_db_controller):
+        with app.app_context():
+            headers, data, user = mock_request_info()
+            mock_db_controller.find_one.return_value = user
+            update_one_result = mock_db_controller.update_one.return_value
+            update_one_result.matched_count = 0
+            result = self._client.put(f'/api/v1/chapter/{str(ObjectId())}', headers=headers, data=json.dumps(data),
+                                      content_type='application/json',
+                                      )
+        self.assertEqual(404, result.status_code)
+
+    @mock.patch('app._db_controller')
+    def test_postChapter_success(self, mock_db_controller):
+        with app.app_context():
+            headers, data, user = mock_request_info()
+            mock_db_controller.find_one.return_value = user
+            insert_one_result = mock_db_controller.insert_one.return_value
+            insert_one_result.return_value = ObjectId()
+            result = self._client.post(f'/api/v1/chapter', headers=headers, data=json.dumps(data),
+                                       content_type='application/json',
+                                       )
+        self.assertEqual(201, result.status_code)
+
+    @mock.patch('app._db_controller')
+    def test_postChapter_failure(self, mock_db_controller):
+        with app.app_context():
+            headers, data, user = mock_request_info()
+            mock_db_controller.find_one.return_value = user
+            mock_db_controller.insert_one.return_value = None
+            result = self._client.post(f'/api/v1/chapter', headers=headers, data=json.dumps(data),
+                                       content_type='application/json',
+                                       )
+        self.assertEqual(500, result.status_code)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_data/chapters_data.py
@@ -0,0 +1,46 @@
+from bson.objectid import ObjectId
+
+
+def mock_chapter_data():
+    return mock_chapters_data()[0]
+
+
+def mock_chapters_data():
+    return [
+        {
+            "_id": ObjectId("62fbacd709932fd2b4d682a4"),
+            "author": "not eli 2",
+            "holyBook": "Old Testemant",
+            "book": "Genesis",
+            "verses": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "summary": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "analysis": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "rating": {
+                "moral": 3,
+                "scientific": 2,
+                "historical": 2
+            },
+            "parentComments": [
+                ""
+            ],
+            "chapter": 1
+        },
+        {
+            "_id": ObjectId("62fbaf7f09932fd2b4d682a5"),
+            "author": "Mark Entelis",
+            "holyBook": "Old Testemant",
+            "book": "Genesis",
+            "verses": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "summary": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "analysis": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "rating": {
+                "moral": 3,
+                "scientific": 2,
+                "historical": 2
+            },
+            "parentComments": [
+                ""
+            ],
+            "chapter": 2
+        }
+    ]
