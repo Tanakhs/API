4472c07209d64ae5cf14c99250faa87be7ddeba9
Eli Entelis <eli.entelis@hexagon.com>
2022-09-05 21:03:36 +0300

    post comment & post nested comment works

--- db_services/db_controller.py
+++ db_services/db_controller.py
@@ -16,8 +16,9 @@ class DbController:
     def insert_one(self, db_name: str, collection_name: str, record: dict) -> ObjectId:
         return self._db_service.insert_one(db_name, collection_name, record)
 
-    def update_one(self, db_name: str, collection_name: str, query: dict, record: dict) -> UpdateResult:
-        return self._db_service.update_one(db_name, collection_name, query, record)
+    def update_one(self, db_name: str, collection_name: str, query: dict, record: dict,
+                   array_filters: dict = {}) -> UpdateResult:
+        return self._db_service.update_one(db_name, collection_name, query, record, array_filters)
 
     def delete_one(self, db_name: str, collection_name: str, record_id) -> DeleteResult:
         return self._db_service.delete_one(db_name, collection_name, record_id)
