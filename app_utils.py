6497d9edd6c6aae82b70a2a3f781a3635347864a
Yael Groisman <yaelgr999@gmail.com>
2023-02-18 00:46:09 +0200

    Fixed checking permission decorator in endpoint

---
+++ app_utils.py
@@ -0,0 +1,33 @@
+import functools
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask import abort
+from inspect import getfullargspec
+
+from models.user import User, Role
+
+
+class permission_required:
+    def __init__(self, permission):
+        self.permission = permission
+
+    def __call__(self, function):
+        @jwt_required()
+        @functools.wraps(function)
+        def wrapped_function(*args, **kwargs):
+            from app import DB_CONTROLLER
+
+            current_user = get_jwt_identity()
+            user_from_db = DB_CONTROLLER.find_one('secular_review', 'users', {'user_name': current_user})
+
+            if not user_from_db:
+                abort(401)
+            user_model = User(**user_from_db)
+
+            if not (user_model.role == self.permission or user_model.role == Role.ADMIN):
+                abort(403)
+
+            if 'current_user' in getfullargspec(function).args:
+                kwargs['current_user'] = user_model
+            return function(*args, **kwargs)
+
+        return wrapped_function
