1d5b31403ff4091a094d9b3d6c7b00d03314d5c0
Yael Groisman <yaelgr999@gmail.com>
2023-02-18 00:45:24 +0200

    Moved all envi variables to config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
+import datetime
 from dotenv import load_dotenv
 
-load_dotenv()  # Load the environment variables from the .env file
+load_dotenv()  # Load the environment variables from the ..env file
 
 
 class Config(object):
@@ -11,11 +12,5 @@ class Config(object):
     CACHE_REDIS_DB = os.environ['CACHE_REDIS_DB']
     CACHE_REDIS_URL = os.environ['CACHE_REDIS_URL']
     CACHE_DEFAULT_TIMEOUT = os.environ['CACHE_DEFAULT_TIMEOUT']
-
-    def __init__(self):
-        print(f"CACHE_TYPE: {self.CACHE_TYPE}")
-        print(f"CACHE_REDIS_HOST: {self.CACHE_REDIS_HOST}")
-        print(f"CACHE_REDIS_PORT: {self.CACHE_REDIS_PORT}")
-        print(f"CACHE_REDIS_DB: {self.CACHE_REDIS_DB}")
-        print(f"CACHE_REDIS_URL: {self.CACHE_REDIS_URL}")
-        print(f"CACHE_DEFAULT_TIMEOUT: {self.CACHE_DEFAULT_TIMEOUT}")
+    JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
+    JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=1)
